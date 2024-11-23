cdcad60b4a3c6b212ff78f05691d1a4c6374f0e2
otari nozadze <otikonozadze@gmail.com>
2024-11-23 21:32:06 +0400

    added logout

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiServiceService } from '../shared/api-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,8 +8,14 @@ import { ApiServiceService } from '../shared/api-service.service';
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent implements OnInit {
-  constructor(private api: ApiServiceService) {}
+  constructor(private api: ApiServiceService, private router: Router) {}
   hotel: string = '';
+
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['login/']);
+  }
+
   ngOnInit(): void {
     this.api.get_hotel().subscribe((data: any) => {
       this.hotel = data['hotel']['name'];
