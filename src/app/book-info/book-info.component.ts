2cbaafba61fc8ccdeee1e58e078c41039a838005
otari nozadze <otikonozadze@gmail.com>
2024-12-03 23:08:04 +0400

    added filter form

--- src/app/book-info/book-info.component.ts
+++ src/app/book-info/book-info.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiServiceService } from '../shared/api-service.service';
-import { Datum, filteredBooked } from '../shared/types';
+import { Datum } from '../shared/types';
 
 @Component({
   selector: 'app-book-info',
